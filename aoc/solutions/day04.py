3abc0736d52d09df7b328a6343297ffa7593bca9
shreenapatel <shreena88@gmail.com>
2022-12-04 13:05:24 +0000

    adding solutions for day 4

---
+++ aoc/solutions/day04.py
@@ -0,0 +1,39 @@
+def part1(data):
+
+    pairs = [tuple(p.split(",")) for p in data.rstrip("\n").split("\n")]
+
+    sections = [
+        [
+            set(range(int(elf1.split("-")[0]), int(elf1.split("-")[1]) + 1)),
+            set(range(int(elf2.split("-")[0]), int(elf2.split("-")[1]) + 1)),
+        ]
+        for elf1, elf2 in pairs
+    ]
+
+    # count how many pairs have full overlap
+    full_overlap = [
+        any([elf1.issubset(elf2), elf2.issubset(elf1)])
+        for elf1, elf2 in sections  # noqa
+    ]
+    num_with_full_overlap = sum(full_overlap)
+
+    return num_with_full_overlap
+
+
+def part2(data):
+
+    pairs = [tuple(p.split(",")) for p in data.rstrip("\n").split("\n")]
+
+    sections = [
+        [
+            set(range(int(elf1.split("-")[0]), int(elf1.split("-")[1]) + 1)),
+            set(range(int(elf2.split("-")[0]), int(elf2.split("-")[1]) + 1)),
+        ]
+        for elf1, elf2 in pairs
+    ]
+
+    # count how many pairs have some overlap
+    overlap = [len(set.intersection(elf1, elf2)) for elf1, elf2 in sections]
+    num_with_overlap = len(overlap) - overlap.count(0)
+
+    return num_with_overlap
---
+++ tests/test_day04.py
@@ -0,0 +1,22 @@
+from aoc.runner import load_data
+from aoc.solutions.day04 import part1, part2  # noqa
+
+
+def test_part1_solution_is_correct():
+    """Check output against known solution"""
+
+    data = load_data(4)
+
+    output = part1(data)
+
+    assert output == 524
+
+
+def test_part2_solution_is_correct():
+    """Check output against known solution"""
+
+    data = load_data(4)
+
+    output = part2(data)
+
+    assert output == 798
